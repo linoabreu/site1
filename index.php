aa06c1f3a562f20b2048df242e9ce61397f3930f
linoabreu <linox_2@hotmail.com>
2022-05-05 21:43:57 -0300

    adicionando validação de campos e excluindo arquivos e pastas não usadas

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário de Inscrição</title>
+</head>
+<body>
+	<p>Formulário para inscrição de Competidores</p>
+	<form action="script.php" method="post">
+		<p>Seu nome: <input type="text" name="nome"></p>
+		<p>Sua idade: <input type="text" name="idade"></p>
+		<p><input type="submit" value="Enviar dados do competidor"></p>
+	</form>
+</body>
+</html>
